ab7a57030325d154938ae58ba717cfc287988c37
Ryan Vennemeyer <51283756+RyanVennemeyer@users.noreply.github.com>
2022-02-10 00:22:07 -0500

    Update alpha.php

--- back/alpha.php
+++ back/alpha.php
@@ -22,6 +22,13 @@ $result=$query->fetchAll();
 if ($result == true){
     
     $sql1="SELECT `Role` FROM UserCS490 WHERE `Username`=`$UCID`";
+    $array = Array (
+    "login" => Array (
+        "UCID" => "$UCID",
+        "Password" => "$Password",
+        "role" => "$sql1"
+    )
+);
     $query1 = $DB->query($sql1);
     $result1=$query1->fetchAll();
     echo "WELCOME". $result1;
@@ -29,4 +36,41 @@ if ($result == true){
     echo "Not Acceptable";
 }
 
+$ch = curl_init();
+$url = "https://afsaccess4.njit.edu/~sk2662/CS490Alpha/testmiddle.php";
+$data = file_get_contents('php://input');
+
+
+
+/*
+<?php
+//Connect to database
+include "DB.php";
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+ $str_json = file_get_contents('php://input');
+	$response = json_decode($str_json, true); // decoding received JSON to array
+//Build query
+$username = $_POST['username'];
+$password = $_POST['password'];
+$query = "SELECT * FROM UserCS490 WHERE Username='$username' AND Password='$password'";
+
+//Build response
+$result = mysqli_query($db,$query);
+$response = array(
+	"Username" => (mysqli_num_rows($result) == 1)
+	);
+$db->close();
+
+//Return response
+
+header("Content-Type:application/json");
+echo json_encode($response);
+
+
 ?>
+*/
+
+?>
+
